43b2c4490c47b189bc1e242ed1e862063214a6a7
bitofsecurity <shakil.islam@gmail.com>
2021-09-22 22:58:08 +1000

    Initial Commit

---
+++ dns-query-from-file.sh
@@ -0,0 +1,22 @@
+!/bin/bash
+# Script file - resolverDNS.sh
+# Checking existence of arg
+if [ "$1" == "" ]
+then
+  # Display help if wrong usage
+  echo "Usage: /bin/bash resolverDNS.sh /path/to/file"
+  exit 35
+else 
+  # Loop over dns and resolve
+  while IFS='' read -r line || [[ -n "$line" ]]; do
+    dns=''
+    # Resolve reverse DNS
+    if [[ $line =~ ^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$ ]]; then
+      dns=`dig +noall +answer -x $line +short|tr 'n' ' '`
+    # Resolve A record
+    else
+      dns=`dig a $line +short|tr 'n' ' '`
+    fi
+    echo -e "$linetis resolving intot${dns}"
+  done < "$1"
+fi
